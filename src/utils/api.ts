4e20e030ce28706f0e9409dbc1264ac54a1b4eed
TeemourA <taxprogramming@gmail.com>
2022-11-08 17:42:43 +0300

    fix env variables names

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,10 @@
 import { localStorageTokenName } from '../constants';
 
 export const getBaseUrl = () => {
-  if (process.env.REACT_APP_MODE === 'development') return process.env.API_DEV_ORIGIN;
+  if (process.env.REACT_APP_MODE === 'development')
+    return process.env.REACT_APP_API_DEV_ORIGIN;
 
-  return process.env.API_PROD_ORIGIN;
+  return process.env.REACT_APP_API_PROD_ORIGIN;
 };
 
 export const getCurrentSessionToken = () => {
